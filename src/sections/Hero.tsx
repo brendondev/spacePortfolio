bd64477201dcfff00fa9307538bf035076e8e9ec
Brendon Lima <47898677+brendondev@users.noreply.github.com>
2024-09-25 10:46:22 -0300

    project section mb md

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,44 +9,46 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 export const HeroSection = () => {
 	return (
 		<div className="py-32 md:py-48 lg:py-60 relative -z-10 overflow-x-clip">
-			<div
-				className="absolute inset-0 z-0 opacity-5"
-				style={{ backgroundImage: `url(${grainImage.src})` }}
-			/>
-			<div className="size-[620px] hero-ring" />
-			<div className="size-[820px] hero-ring" />
-			<div className="size-[1020px] hero-ring" />
-			<div className="size-[1220px] hero-ring" />
-			<HeroOrbit size={800} rotation={-72}>
-				<StarIcon className="size-28 text-emerald-300" />
-			</HeroOrbit>
-			<HeroOrbit size={550} rotation={20}>
-				<StarIcon className="size-12 text-emerald-300" />
-			</HeroOrbit>
-			<HeroOrbit size={590} rotation={98}>
-				<StarIcon className="size-8 text-emerald-300" />
-			</HeroOrbit>
-			<HeroOrbit size={430} rotation={-14}>
-				<SparkleIcon className="size-8 text-emerald-300/20" />
-			</HeroOrbit>
-			<HeroOrbit size={440} rotation={79}>
-				<SparkleIcon className="size-5 text-emerald-300/20" />
-			</HeroOrbit>
-			<HeroOrbit size={440} rotation={79}>
-				<SparkleIcon className="size-5 text-emerald-300/20" />
-			</HeroOrbit>
-			<HeroOrbit size={710} rotation={144}>
-				<SparkleIcon className="size-14 text-emerald-300/20" />
-			</HeroOrbit>
-			<HeroOrbit size={720} rotation={85}>
-				<div className="size-3 bg-emerald-300/20 rounded-full" />
-			</HeroOrbit>
-			<HeroOrbit size={520} rotation={-41}>
-				<div className="size-2 bg-emerald-300/20 rounded-full" />
-			</HeroOrbit>
-			<HeroOrbit size={650} rotation={-50}>
-				<div className="size-2 bg-emerald-300/20 rounded-full" />
-			</HeroOrbit>
+			<div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+				<div
+					className="absolute inset-0 z-0 opacity-5"
+					style={{ backgroundImage: `url(${grainImage.src})` }}
+				/>
+				<div className="size-[620px] hero-ring" />
+				<div className="size-[820px] hero-ring" />
+				<div className="size-[1020px] hero-ring" />
+				<div className="size-[1220px] hero-ring" />
+				<HeroOrbit size={800} rotation={-72}>
+					<StarIcon className="size-28 text-emerald-300" />
+				</HeroOrbit>
+				<HeroOrbit size={550} rotation={20}>
+					<StarIcon className="size-12 text-emerald-300" />
+				</HeroOrbit>
+				<HeroOrbit size={590} rotation={98}>
+					<StarIcon className="size-8 text-emerald-300" />
+				</HeroOrbit>
+				<HeroOrbit size={430} rotation={-14}>
+					<SparkleIcon className="size-8 text-emerald-300/20" />
+				</HeroOrbit>
+				<HeroOrbit size={440} rotation={79}>
+					<SparkleIcon className="size-5 text-emerald-300/20" />
+				</HeroOrbit>
+				<HeroOrbit size={440} rotation={79}>
+					<SparkleIcon className="size-5 text-emerald-300/20" />
+				</HeroOrbit>
+				<HeroOrbit size={710} rotation={144}>
+					<SparkleIcon className="size-14 text-emerald-300/20" />
+				</HeroOrbit>
+				<HeroOrbit size={720} rotation={85}>
+					<div className="size-3 bg-emerald-300/20 rounded-full" />
+				</HeroOrbit>
+				<HeroOrbit size={520} rotation={-41}>
+					<div className="size-2 bg-emerald-300/20 rounded-full" />
+				</HeroOrbit>
+				<HeroOrbit size={650} rotation={-50}>
+					<div className="size-2 bg-emerald-300/20 rounded-full" />
+				</HeroOrbit>
+			</div>
 			<div className="container">
 				<div className="flex flex-col items-center">
 					<Image
